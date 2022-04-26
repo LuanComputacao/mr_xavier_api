d5f816cf1d4bf634ee8bc86801c16ba84922638a
LuanComputacao <eng.luanroger@gmail.com>
2022-04-26 18:23:12 -0300

    feat: first commit

---
+++ src/main/java/com/luancomputacao/mr_xavier_api/MrXavierApiApplication.java
@@ -0,0 +1,13 @@
+package com.luancomputacao.mr_xavier_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrXavierApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MrXavierApiApplication.class, args);
+    }
+
+}

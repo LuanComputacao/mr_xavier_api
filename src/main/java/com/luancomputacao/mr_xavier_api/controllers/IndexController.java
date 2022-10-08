2b8907a555a786d21d5d2cf716873a73ec410b2c
LuanComputacao <eng.luanroger@gmail.com>
2022-10-08 17:00:36 -0300

    feat: #1 add index page and controller

---
+++ src/main/java/com/luancomputacao/mr_xavier_api/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package com.luancomputacao.mr_xavier_api.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String index() {
+        return "index";
+    }
+}
